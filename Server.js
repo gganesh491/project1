66ce463d77a3a901a52b5a596177f3e2067674be
gganesh491 <ganeshghorpade491@gmail.com>
2025-01-21 21:03:57 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose")
+mongoose.set('strictQuery', true);
+const express = require("express");
+const cors = require("cors");
+dotenv = require("dotenv").config();
+
+const app = express();
+
+
+const dburl = process.env.DB_URL
+mongoose.connect(dburl).then(() => {
+    console.log("Connected to DB Successfully")
+}).catch((err) => {
+    console.log(err.message)
+});
+
+
+app.use(cors());
+app.use(express.json());
+
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const songRoutes = require("./routes/songs");
+const playListRoutes = require("./routes/playLists");
+const searchRoutes = require("./routes/search");
+
+
+app.use("/api/users/", userRoutes);
+app.use("/api/login/", authRoutes);
+app.use("/api/songs/", songRoutes);
+app.use("/api/playlists/", playListRoutes);
+app.use("/api/search/", searchRoutes);
+
+const port= process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`Server is running at port http://localhost:${port}`)
+})
